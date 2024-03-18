abf5ee5813f63517ec6b4a3785b80979e87982b8
Pungitius <robert.kosmehl@gmx.de>
2024-03-18 16:01:30 +0100

    BS and AABB

--- src/objects/Mesh.js
+++ src/objects/Mesh.js
@@ -155,45 +155,45 @@ class Mesh extends Object3D {
 
 		if ( material === undefined ) return;
 
-		// // test with bounding sphere in world space
+		// test with bounding sphere in world space
 
-		// if ( geometry.boundingSphere === null ) geometry.computeBoundingSphere();
+		if ( geometry.boundingSphere === null ) geometry.computeBoundingSphere();
 
-		// _sphere.copy( geometry.boundingSphere );
-		// _sphere.applyMatrix4( matrixWorld );
+		_sphere.copy( geometry.boundingSphere );
+		_sphere.applyMatrix4( matrixWorld );
 
-		// // check distance from ray origin to bounding sphere
+		// check distance from ray origin to bounding sphere
 
-		// _ray.copy( raycaster.ray ).recast( raycaster.near );
+		_ray.copy( raycaster.ray ).recast( raycaster.near );
 
-		// if ( _sphere.containsPoint( _ray.origin ) === false ) {
+		if ( _sphere.containsPoint( _ray.origin ) === false ) {
 
-		// 	if ( _ray.intersectSphere( _sphere, _sphereHitAt ) === null ) return;
+			if ( _ray.intersectSphere( _sphere, _sphereHitAt ) === null ) return;
 
-		// 	if ( _ray.origin.distanceToSquared( _sphereHitAt ) > ( raycaster.far - raycaster.near ) ** 2 ) return;
-
-		// }
-
-		// // convert ray to local space of mesh
-
-		// _inverseMatrix.copy( matrixWorld ).invert();
-		// _ray.copy( raycaster.ray ).applyMatrix4( _inverseMatrix );
-
-		// // test with bounding box in local space
-
-		// if ( geometry.boundingBox !== null ) {
-
-		// 	if ( _ray.intersectsBox( geometry.boundingBox ) === false ) return;
-
-		// }
-
-		// k-DOP step
-
-		if ( geometry.boundingDOP14 !== null ) {
-
-			if ( geometry.boundingDOP14.intersectsRay(_ray) === false ) return;
+			if ( _ray.origin.distanceToSquared( _sphereHitAt ) > ( raycaster.far - raycaster.near ) ** 2 ) return;
 
 		}
+
+		// convert ray to local space of mesh
+
+		_inverseMatrix.copy( matrixWorld ).invert();
+		_ray.copy( raycaster.ray ).applyMatrix4( _inverseMatrix );
+
+		// test with bounding box in local space
+
+		if ( geometry.boundingBox !== null ) {
+
+			if ( _ray.intersectsBox( geometry.boundingBox ) === false ) return;
+
+		}
+
+		// // k-DOP step
+
+		// if ( geometry.boundingDOP14 !== null ) {
+
+		// 	if ( geometry.boundingDOP14.intersectsRay(_ray) === false ) return;
+
+		// }
 
 		// test for intersections with geometry
 
